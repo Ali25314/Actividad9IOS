bd921963a9761aeb4030774c8b58a534a342641d
Ali25314 <78836439+Ali25314@users.noreply.github.com>
2021-04-05 21:31:47 -0500

    Add files via upload

---
+++ Actividad9IOS/Actividad9IOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Actividad9IOS
+//
+//  Created by user166430 on 4/5/21.
+//  Copyright © 2021 user166430. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
